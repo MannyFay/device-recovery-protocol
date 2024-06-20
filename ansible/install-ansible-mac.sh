ec78cb3e4da4442e2b30ae70badeed0845f0a315
Manny Fay <mannythefay@gmail.com>
2024-06-21 01:02:57 +0200

    Update install-ansible-mac.sh

--- ansible/install-ansible-mac.sh
+++ ansible/install-ansible-mac.sh
@@ -1,4 +1,12 @@
-!#/usr/bin/env bash
+#!/bin/zsh
 
+#-- Install Xcode command line tools:
+xcode-select --install
+
+#-- Install Homebrew package manager:
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Volumes/Users/manny/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+#-- Install Ansible:
 brew install ansible
